c5c9831524282b8b051008f00e7f6d0c379f78e3
Siddharth Jain <85589093+sjain166@users.noreply.github.com>
2022-07-28 10:18:15 -0700

    Update connect.py

--- connect.py
+++ connect.py
@@ -1,1 +1,16 @@
+import network, esp, gc
 
+gc.collect()
+esp.osdebug(None)
+
+ssid='jacksparrow'  #Network Name
+password='Password'  #Network Password
+
+station=network.WLAN(network.STA_IF)
+station.active(True)
+station.connect(ssid,password)
+
+while station.isconnected()==False:
+    pass
+print('connection succesful')
+print(station.ifconfig())
